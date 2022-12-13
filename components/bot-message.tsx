7d9216e0443d3e53172a290e1c76a9c0c73d33c7
Chase Struse <chasestruse@gmail.ocm>
2022-12-13 17:14:15 -0600

    Continuing updates

--- components/bot-message.tsx
+++ components/bot-message.tsx
@@ -2,11 +2,17 @@ type BotMessageProps = {
     userSelection: string;
 }
 
+messageContent: '';
+
+const SendMessage = () => {
+    return 'Testing'
+}
+
 export default function BotMessage(props: BotMessageProps) {
     return(
         <div>
             <p>{props.userSelection}</p>
-            <p>Test</p>
+            <p>{SendMessage()}</p>
         </div>
     )
 }
